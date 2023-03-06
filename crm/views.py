ae3d3580ec51900080406143f244ddfaef2d09bc
semen <adm@adm.com>
2023-03-07 00:08:42 +0300

    2023_03_07

--- crm/views.py
+++ crm/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from.models import Order
+from .forms import OrderForm
+from cms.models import CmsSlider
 
 # Create your views here.
 def first_page(request):
-    object_list = Order.objects.all()
-    return render(request, './index.html', { 'object_list': object_list})
+    slider_list = CmsSlider.objects.all()
+    return render(request, './index.html', { 'slider_list': slider_list,
+                                             })
+
+def thanks_page(request):
+    name = request.GET['name']
+    phone = request.GET['phone']
+    element = Order(order_name = name, order_phone = phone)
+    element.save()
+    return render(request, './thanks_page.html', {'name': name,
+                                                  'phone': phone})
